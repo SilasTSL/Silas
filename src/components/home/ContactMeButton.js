5e1e4fc0b1976a3902e9f6299320d307e5cdb1ec
SilasTSL <silastaysl@gmail.com>
2022-12-24 00:43:56 +0700

    Add home page

---
+++ src/components/home/ContactMeButton.js
@@ -0,0 +1,11 @@
+import styles from './ContactMeButton.module.css';
+
+import React from 'react'
+
+export const ContactMeButton = () => {
+    return (
+        <div className={styles.contactMeButtonContainer}>
+            Contact Me
+        </div>
+    )
+}
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,16 @@
 import styles from './Home.module.css';
 
 import React from 'react'
+import { ContactMeButton } from '../../components/home/ContactMeButton';
 
 export const Home = () => {
     return (
         <div className={styles.homeContainer}>
-            <h1 id="home">THIS IS HOME</h1>
+            <h2 id="home" className={styles.homeIntro}>Nice to meet you, I'm</h2>
+            <h1 className={styles.homeName}>Silas Tay</h1>
+            <h3 className={styles.homeSubtitle}>Computer Scientist <span className={styles.homeSubtitleDivider}>.</span> Software Engineer <span className={styles.homeSubtitleDivider}>.</span> Passionate Entrepreneur</h3>
+            <p className={styles.homeDescription}>I'm a Year 2 student studying Computer Science in the National University of Singapore. I am deeply interested in fields such as Software Engineering, Web Development and Artificial Intelligence, while also being an Aspiring Entrepreneur looking to change the lives of people around the globe.</p>
+            <ContactMeButton />
         </div>
     )
 }
