de8dcd657d162deed99c7055eb1e55445cf8f440
SilasTSL <silastaysl@gmail.com>
2023-01-01 18:35:49 +0800

    Fix wrapping bug in experience component

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -25,9 +25,9 @@ export const Experience = () => {
                     <div className={styles.experienceExperienceBodyContainer}>
                         <h4 className={styles.experienceExperienceTimeline}><span className={styles.highlight}>Data Intern</span> from 2021 April - August</h4>
                         <div className={styles.experienceExperienceDetails}>
-                            <p className={styles.experienceExperienceDetailPointer}><img className={styles.experienceListIcon} src={require("../../images/list-icon.png")}/>Built programs to scrape dynamic and static websites (<span className={styles.highlight}>Python</span>, <span className={styles.highlight}>&nbsp;BeautifulSoup</span>, <span className={styles.highlight}>&nbsp;Selenium</span>, <span className={styles.highlight}>&nbsp;Docker</span>)</p>
-                            <p className={styles.experienceExperienceDetailPointer}><img className={styles.experienceListIcon} src={require("../../images/list-icon.png")}/>Built robust data collection pipelines to feed company databases (<span className={styles.highlight}>GCP</span>, <span className={styles.highlight}>&nbsp;PostgreSQL</span>)</p>
-                            <p className={styles.experienceExperienceDetailPointer}><img className={styles.experienceListIcon} src={require("../../images/list-icon.png")}/>Organised databases to meet client’s requirements</p>
+                            <div className={styles.experienceExperienceDetailPointer}><img className={styles.experienceListIcon} src={require("../../images/list-icon.png")}/><p>Built programs to scrape dynamic and static websites (<span className={styles.highlight}>Python</span>, <span className={styles.highlight}>&nbsp;BeautifulSoup</span>, <span className={styles.highlight}>&nbsp;Selenium</span>, <span className={styles.highlight}>&nbsp;Docker</span>)</p></div>
+                            <div className={styles.experienceExperienceDetailPointer}><img className={styles.experienceListIcon} src={require("../../images/list-icon.png")}/><p>Built robust data collection pipelines to feed company databases (<span className={styles.highlight}>GCP</span>, <span className={styles.highlight}>&nbsp;PostgreSQL</span>)</p></div>
+                            <div className={styles.experienceExperienceDetailPointer}><img className={styles.experienceListIcon} src={require("../../images/list-icon.png")}/><p>Organised databases to meet client’s requirements</p></div>
                         </div>
                     </div>
                 </div>
@@ -37,10 +37,10 @@ export const Experience = () => {
                     <div className={styles.experienceExperienceBodyContainer}>
                         <h4 className={styles.experienceExperienceTimeline}><span className={styles.highlight}>Project Striker</span> from 2022 May - August</h4>
                         <div className={styles.experienceExperienceDetails}>
-                            <p className={styles.experienceExperienceDetailPointer}><img className={styles.experienceListIcon} src={require("../../images/list-icon.png")}/>Built Striker, a task-managing application driven with data analytics</p>
-                            <p className={styles.experienceExperienceDetailPointer}><img className={styles.experienceListIcon} src={require("../../images/list-icon.png")}/>Clean and Seamless UI build with <span className={styles.highlight}>&nbsp;React</span></p>
-                            <p className={styles.experienceExperienceDetailPointer}><img className={styles.experienceListIcon} src={require("../../images/list-icon.png")}/>Backend with <span className={styles.highlight}>&nbsp;Golang</span></p>
-                            <p className={styles.experienceExperienceDetailPointer}><img className={styles.experienceListIcon} src={require("../../images/list-icon.png")}/>Login Authentication with <span className={styles.highlight}>&nbsp;Firebase</span></p>
+                            <div className={styles.experienceExperienceDetailPointer}><img className={styles.experienceListIcon} src={require("../../images/list-icon.png")}/><p>Built Striker, a task-managing application driven with data analytics</p></div>
+                            <div className={styles.experienceExperienceDetailPointer}><img className={styles.experienceListIcon} src={require("../../images/list-icon.png")}/><p>Clean and Seamless UI build with <span className={styles.highlight}>&nbsp;React</span></p></div>
+                            <div className={styles.experienceExperienceDetailPointer}><img className={styles.experienceListIcon} src={require("../../images/list-icon.png")}/><p>Backend with <span className={styles.highlight}>&nbsp;Golang</span></p></div>
+                            <div className={styles.experienceExperienceDetailPointer}><img className={styles.experienceListIcon} src={require("../../images/list-icon.png")}/><p>Login Authentication with <span className={styles.highlight}>&nbsp;Firebase</span></p></div>
                         </div>
                     </div>
                 </div>
